99ad89eb5f76dec922717d3aa969d10058cafcf1
unknown <ismail.ouali@ump.ac.ma>
2024-10-22 20:06:13 +0100

    Ex from 1 - 8 from w3cressources

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        Ex 1
+        List<String> list = new ArrayList<String>();
+
+        list.add("Yellow");
+        list.add("blue");
+        list.add("Red");
+        list.add("Green");
+
+        System.out.println(list);
+/*
+        Ex 2
+
+        for(String color : list){
+           System.out.println(color);
+        }
+
+        Ex 3
+
+        list.add(0,"Gray");
+
+        Ex 4
+*/
+
+        list.get(1);
+
+//        Ex 5
+
+//        list.set(1, "White");
+
+//        Ex 6
+//        list.remove(2);
+
+//        Ex 7
+//        list.contains("Yellow");
+
+
+//        Ex 8
+
+//        System.out.println(list.stream().sorted());
+    }
+
+//    Ex 9
+
+
+
+
+}
